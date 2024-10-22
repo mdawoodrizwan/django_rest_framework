cc38cb7c640cf445db7236ae28f38512f625d2a1
mdawoodrizwan <dawood.rizwan@kryptomind.com>
2024-10-22 16:04:56 +0500

    Fix issues

--- ToDoList/mainApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-class ToDoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ToDo
-        fields = ('id', 'title', 'description', 'Date', 'Completed') 
